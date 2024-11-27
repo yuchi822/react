71e1512075ade7a2befbd0dd64ba6cb2c16eb237
tony0831-L <64834890+tony0831-L@users.noreply.github.com>
2024-11-28 01:36:26 +0800

    feat(api): findAll demo 接 findAll api, 渲染

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,35 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import '../style/App.css'
-import { WaterQualityData } from '../interface/WaterQualityData'
-
-type WaterQualityResp = {
-  httpCode: number,
-  httpMessage: string,
-  count: number,
-  item: Array<WaterQualityData>
-}
+import { asyncGet } from '../utils/fetch'
+import { api } from '../enum/api'
+import { Student } from '../interface/Student'
+import { resp } from '../interface/resp'
 
 function App() {
 
-  const [waterQualityState, setWaterQuality] = useState<WaterQualityResp>()
+  const [students, setStudents] = useState<Array<Student>>([])
+
+  const cache = useRef<boolean>(false)
 
   useEffect(() => {
-    fetch("https://api.water.gov.taipei/prod/WaterQualityData", {
-      method: "Post"
-    }).then(res => {
-      res.json().then((res: WaterQualityResp) => {
-        setWaterQuality(res)
-      })
-    })
+    /**
+     * 做緩存處理, 避免多次發起請求
+     */
+    if (!cache.current) {
+      cache.current = true;
+      asyncGet(api.findAll).then((res: resp<Array<Student>>) => {
+        if (res.code == 200) {
+          setStudents(res.body)
+        }
+      });
+    }
   }, [])
 
-  const waterQualities = waterQualityState ? waterQualityState.item.map((waterQualityData: WaterQualityData) => {
+  const studentList = students ? students.map((student: Student) => {
     return (
-      <div className='waterQuality'>
-        <h1>{waterQualityData.code_name}</h1>
-        <p>濁度: {waterQualityData.qua_cntu}</p>
-        <p>餘氯: {waterQualityData.qua_cl}</p>
-        <p>ph值: {waterQualityData.qua_ph}</p>
+      <div className='student' key={student._id}>
+        <p>帳號: {student.userName}</p>
+        <p>座號: {student.sid}</p>
+        <p>姓名: {student.name}</p>
+        <p>院系: {student.department}</p>
+        <p>年級: {student.grade}</p>
+        <p>班級: {student.class}</p>
+        <p>Email: {student.Email}</p>
+        <p>缺席次數: {student.absences ? student.absences : 0}</p>
       </div>
     )
   }) : "loading"
@@ -37,7 +43,7 @@ function App() {
   return (
     <>
       <div className="container">
-        {waterQualities}
+        {studentList}
       </div>
     </>
   )
